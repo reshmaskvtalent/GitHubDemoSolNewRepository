2092c6802a12eb3b688e25e215af38977fdf737f
reshmaskvtalent <160871340+reshmaskvtalent@users.noreply.github.com>
2024-02-22 20:36:16 +0530

    Update Sample.cs

--- GitHubDemoProjectNew/Models/Sample.cs
+++ GitHubDemoProjectNew/Models/Sample.cs
@@ -17,5 +17,11 @@ namespace GitHubDemoProjectNew.Models
         {
             return x - y;
         }
+
+         public int Mul(int x, int y)
+        {
+            return x * y;
+        }
+        
     }
 }
